ac18ed42332cd2250c4c84ea66bfc67c7a99a9f5
An_Peihao <an_peihao@peihaoan.local>
2019-08-05 19:53:25 +0800

    adjust config

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div id="container">
+    </div>
   </div>
 </template>
 
@@ -12,17 +14,45 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  mounted(){
+    let that = this
+    that.fn_resize()
+  },
+  methods: {
+    fn_resize() {
+      // var device_width = document.documentElement.clientWidth || window.innerWidth;
+      var device_width = $('#container').width()
+      var device_height = device_width / 7680 * 3240
+      $('#container').height(device_height)
+
+      // let font_size = device_height / 1620 * 18
+
+      // let html = document.getElementsByTagName('html')[0]
+      // html.style.font_size = font_size + 'px'
+    },
   }
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  // background-color: blue;
+  #container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    // background-color: red;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// add
+import $ from 'jquery'
+window.$ = $
+
+import * as d3 from 'd3'
+window.d3 = d3
+// end add --------------------------
+
 Vue.config.productionTip = false
 
 new Vue({
